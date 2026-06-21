6501cdd18b34cdb354ff7bf42877660585a7925e
omar <omarkallel93@gmail.com>
2026-06-21 03:57:05 +0100

    Add admin dashboard and delete searches

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\PostCheck;
+
 use App\Repository\PostCheckRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -27,6 +28,13 @@ class AdminController extends AbstractController
             'latestChecks' => $postCheckRepository->findBy([], ['createdAt' => 'DESC'], 20),
         ]);
     }
+    #[Route('/admin/check/{id}', name: 'app_admin_check_show', methods: ['GET'])]
+public function showCheck(PostCheck $postCheck): Response
+{
+    return $this->render('check/show.html.twig', [
+        'postCheck' => $postCheck,
+    ]);
+}
 
     #[Route('/admin/check/{id}/delete', name: 'app_admin_check_delete', methods: ['POST'])]
     public function deleteCheck(
