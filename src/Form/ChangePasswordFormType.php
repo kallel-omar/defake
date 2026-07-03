44728533301fb4566ea1dacf6096bed11cafc71a
omar <omarkallel93@gmail.com>
2026-07-03 18:54:23 +0100

    Relax reset password validation

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -10,7 +10,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
-use Symfony\Component\Validator\Constraints\PasswordStrength;
 
 class ChangePasswordFormType extends AbstractType
 {
@@ -28,11 +27,10 @@ class ChangePasswordFormType extends AbstractType
                     'constraints' => [
                         new NotBlank(message: 'Please enter a password.'),
                         new Length(
-                            min: 12,
+                            min: 8,
                             minMessage: 'Your password should be at least {{ limit }} characters.',
                             max: 4096,
                         ),
-                        new PasswordStrength(minScore: PasswordStrength::STRENGTH_MEDIUM),
                         new NotCompromisedPassword(message: 'This password has appeared in a data breach. Please choose a different one.'),
                     ],
                     'label' => 'New password',
